8e2c0468bc87e464de8e68ee719e81b04c2d5e59
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-14 18:24:42 +0200

    try-except in get version from git

--- expipe/version.py
+++ expipe/version.py
@@ -1,5 +1,8 @@
-import git
 import os.path as op
-ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
-repo = git.Repo(ROOT_DIR)
-version = repo.git.describe(tags=True)
+try:
+    import git
+    ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
+    repo = git.Repo(ROOT_DIR)
+    version = repo.git.describe(tags=True)
+except Exception:
+    version = ''
