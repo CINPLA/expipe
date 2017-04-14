ac1a6bbebec930c242479d9accdca476c762e9ca
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-14 19:29:52 +0200

    gitpython dep

--- expipe/version.py
+++ expipe/version.py
@@ -1,6 +1,6 @@
 import os.path as op
+import git
 try:
-    import git
     ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
     repo = git.Repo(ROOT_DIR)
     version = repo.git.describe(tags=True)
