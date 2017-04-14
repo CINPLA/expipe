f279c7811bcf9381bdd6fe067349bdc5971e33f3
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-14 12:52:51 +0200

    bugfix

--- expipe/version.py
+++ expipe/version.py
@@ -1,5 +1,5 @@
 import git
 import os.path as op
-ROOT_DIR = op.dirname(op.abspath(__file__))
+ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
 repo = git.Repo(ROOT_DIR)
 version = repo.git.describe()
