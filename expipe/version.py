e38ab359e03236837375deff31cc5294afa92dde
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-14 13:03:14 +0200

    tagged version

--- expipe/version.py
+++ expipe/version.py
@@ -2,4 +2,4 @@ import git
 import os.path as op
 ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
 repo = git.Repo(ROOT_DIR)
-version = repo.git.describe()
+version = repo.git.describe(tags=True)
