f0d41dd911bcb6cc3d4e561a59cf3af68e066ea5
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-18 09:31:26 +0200

    remove buggy gitpython

--- expipe/version.py
+++ expipe/version.py
@@ -1,8 +1,1 @@
-import os.path as op
-import git
-try:
-    ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
-    repo = git.Repo(ROOT_DIR)
-    version = repo.git.describe(tags=True)
-except Exception:
-    version = ''
+version = '0.3.8'
