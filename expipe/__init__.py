b80720c99d1ed3fafd23bc97b997b3e256a86d0c
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-26 17:14:45 +0200

    add require and get template

--- expipe/__init__.py
+++ expipe/__init__.py
@@ -1,5 +1,6 @@
 from ._version import get_versions
 from .config import configure, ensure_testing
-from .io.core import get_project, require_project, delete_project
+from .io.core import (get_project, require_project, delete_project,
+                      get_template, require_template)
 __version__ = get_versions()['version']
 del get_versions
