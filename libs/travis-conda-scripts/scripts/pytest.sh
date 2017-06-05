0f7d06adda0659d240b2a088c113e27018b874ac
Mikkel E Lepperød <lep.mik@gmail.com>
2017-06-05 09:37:45 +0200

    coveralls

--- libs/travis-conda-scripts/scripts/pytest.sh
+++ libs/travis-conda-scripts/scripts/pytest.sh
@@ -1,2 +1,3 @@
 pip install pytest pytest-cov pytest-pep8 coveralls pyrebase
+python setup.py install
 py.test expipe/io/tests --doctest-modules --doctest-glob='*.rst' --pep8 expipe -v --cov expipe --cov-report term-missing
