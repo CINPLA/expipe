316c271dc4b10a7291c754b32d98cc3aafaa89d9
Mikkel E Lepperød <lep.mik@gmail.com>
2017-06-06 09:34:08 +0200

    try codecov

--- libs/travis-conda-scripts/scripts/pytest.sh
+++
@@ -1,3 +0,0 @@
-pip install pytest pytest-cov pytest-pep8 coveralls pyrebase quantities numpy
-python setup.py install
-py.test expipe/io/tests --doctest-modules --doctest-glob='*.rst' --pep8 expipe -v --cov expipe --cov-report term-missing
