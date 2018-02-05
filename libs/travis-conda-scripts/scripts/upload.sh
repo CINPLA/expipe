52e94f85d058197653b3ed333f8718290a1df1dd
Milad <miladh@ibv.uio.no>
2018-02-05 10:15:39 +0100

    git subrepo commit libs/travis-conda-scripts
    subrepo:
      subdir:   "libs/travis-conda-scripts"
      merged:   "02e37cc"
    upstream:
      origin:   "git@github.com:CINPLA/travis-conda-scripts"
      branch:   "master"
      commit:   "02e37cc"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "3525fd9"

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -4,23 +4,14 @@ if [ -z "$1" ]; then
     exit
 fi
 if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
-    echo "Package name $PACKAGE"
-    conda convert "$PACKAGE" --platform win-64 -o packages --force
-    conda convert "$PACKAGE" --platform osx-64 -o packages --force
-    conda convert "$PACKAGE" --platform linux-64 -o packages --force
-    cd packages
     LABEL=${2:-main}
-    for os in $(ls); do
-        cd $os
-        for package in $(ls); do
-            echo "Uploading $package to anaconda with anaconda upload..."
-            set +x # hide token
-            anaconda -t "$CONDA_UPLOAD_TOKEN" upload -u "$1" --force "$package" -l "$LABEL"
-            set -x
-        done
-        cd ..
+
+    for TARBALL in $CONDA_BLD_PATH/noarch/*.tar.bz2; do
+        echo "Uploading $TARBALL to anaconda with anaconda upload..."
+        set +x # hide token
+        anaconda -t "$CONDA_UPLOAD_TOKEN" upload -u "$1" --force "$TARBALL" -l "$LABEL"
+        set -x
     done
-    cd ..
     echo "Upload command complete!"
 else
     echo "Upload cancelled due to failed test."
