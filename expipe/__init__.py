d2c178dd97902835f0ad349c57c733d622506448
Svenn-Arne Dragly <s@dragly.com>
2018-10-25 17:57:51 +0200

    Make sure deprecation warnings are printed.

--- expipe/__init__.py
+++ expipe/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+import re
 from ._version import get_versions
 from .config import settings
 from .core import load_file_system, load_firebase, require_project
@@ -5,3 +7,8 @@ from . import backends
 # __version__ = get_versions()['version']
 __version__ = "1.0"
 del get_versions
+
+# DeprecationWarning should always be printed
+warnings.filterwarnings(
+    'always', category=DeprecationWarning,
+    module=r'^{0}\.'.format(re.escape(__name__)))
